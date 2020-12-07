11a3c081bfbe65e0a9308ad6c798e02e25246186
Łukasz <aadamczak@edu.cdv.pl>
2020-12-07 21:24:42 +0100

    123

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const yup = require('yup');
 const { uptime } = require('process');
 const helmet = require("helmet");
 const express = require('express');
+const path = require('path');
+const formidable = require('formidable');
+const fs = require('fs');
 var bodyParser = require('body-parser');
 const app = express()
 const port = 3000
@@ -61,9 +64,41 @@ app.get('/hello/:name', function (req,res){
 
 
 // 3 
-var tab = [];
+var tab1 = [];
 app.post('/store', function (req,res) {
-  tab.push(req.body.input);
+  tab1.push(req.body.input);
   res.status(201);
-  res.json({stored_data: tab});
+  res.json({stored_data: tab1});
 })
+
+var tab2 = [];
+
+app.post('/parse', function(req,res) {
+  const form = 'C:/Users/Solda/web_services/file.txt';
+  //const form = formidable({multiples: true});
+  //path.basename(form) 
+  form.parse(req, (err, fields, files) => {
+    fs.readFile(files.file.path, function read(err, data) {
+       const values = data.toString().trim().split(";");
+       const result = [];
+       values.forEach((item) => {
+         const parsedItem = item.replace(":",";");
+         const [key, value] = parsedItem.split(";");
+         const parsedValue = Number(value);
+         if (!Number.isNaN(parsedValue)) {
+          result[key] = parsedValue;
+        } else {
+          const subValues = value.split(",");
+          result[key] = {};
+          subValues.forEach((subItem) => {
+            const [subKey, subValue] = subItem.split(":");
+            result[key][subKey] = Number(subValue);
+          });
+        }
+       });
+      //var buff = Buffer.from(file);
+      res.json(result);
+      res.status(200);
+    });
+  });
+});
